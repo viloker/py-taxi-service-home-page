4515dbbc41e719b3b838c6619c793998a16b3a2d
viloker <yarmolykvadym@gmail.com>
2026-02-11 06:18:18 +0100

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
-from .models import Car, Driver, Manufacturer
+from taxi.models import Car, Driver, Manufacturer
 
 
 def index(requests: HttpRequest) -> HttpResponse:
@@ -9,7 +9,9 @@ def index(requests: HttpRequest) -> HttpResponse:
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
 
-    return render(requests, "taxi/index.html",
-                  {"num_drivers": num_drivers,
-                   "num_manufacturers": num_manufacturers,
-                   "num_cars": num_cars})
+    return render(
+        requests, "taxi/index.html",
+        {"num_drivers": num_drivers,
+         "num_manufacturers": num_manufacturers,
+         "num_cars": num_cars}
+    )
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "taxi_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static", ]
 
 # Default primary key field type
