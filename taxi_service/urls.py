bb28523031cec1723643c58be20970b58991b4a8
viloker <yarmolykvadym@gmail.com>
2026-02-11 06:37:23 +0100

    Solution

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("taxi.urls", namespace="taxi"), name="")
+    path("", include("taxi.urls", namespace="taxi"))
 ]
