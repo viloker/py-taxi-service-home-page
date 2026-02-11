eb0f1f10160c02f1e6569de284ae637770bdf47a
viloker <yarmolykvadym@gmail.com>
2026-02-11 05:57:36 +0100

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+from .models import Car, Driver, Manufacturer
+
+
+def index(requests: HttpRequest) -> HttpResponse:
+    num_drivers = Driver.objects.count()
+    num_manufacturers = Manufacturer.objects.count()
+    num_cars = Car.objects.count()
+
+    return render(requests, "taxi/index.html",
+                  {"num_drivers": num_drivers,
+                   "num_manufacturers": num_manufacturers,
+                   "num_cars": num_cars})
--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "taxi_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+ BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
